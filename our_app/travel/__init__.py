828a85237aa986da8fb883f3bedded88fc35002e
mattmrm <147670506+mattmrm@users.noreply.github.com>
2023-10-24 21:17:48 +1000

    importing travel forms
    importing travels forms

--- our_app/travel/__init__.py
+++ our_app/travel/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import datetime
+from travel.forms import SearchForm
 
 db = SQLAlchemy()
 app = Flask(__name__)
